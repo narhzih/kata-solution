6f9d7fc9db1d8b814d966daba3fbe4ef9fe47c31
Symfony <omosekejiolawale@gmail.com>
2022-08-23 10:45:31 +0100

    Changed from js to ts

---
+++ dist/Converter.d.ts
@@ -0,0 +1,10 @@
+export default class Converter {
+    private units;
+    private tensPrefix;
+    private getNumberPlaceValue;
+    private getArraySum;
+    convertDigit(digit: any): any;
+    private convertUnit;
+    private convertTens;
+    private convertHundredAndMore;
+}
---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Converter_1 = require("./Converter");
+exports.default = Converter_1.default;
