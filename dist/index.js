67edf0086cc3b446445c64620c542b5cfbcafd98
Symfony <omosekejiolawale@gmail.com>
2022-08-23 12:20:32 +0100

    Adjustments

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Converter_1 = require("./Converter");
+console.log(new Converter_1.default().convertDigits([1, 2, 4]));
 exports.default = Converter_1.default;
