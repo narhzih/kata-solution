fb024a17e2e5a6343832aaa1e3c9781a98f7bdba
Symfony <omosekejiolawale@gmail.com>
2022-08-23 08:25:28 +0100

    Added helper function to split number into unit, tens, hundered and more...

--- Converter.js
+++ Converter.js
@@ -36,6 +36,15 @@ class Converter {
     return this.units[digit];
   }
 
+  getNumberPlaceValue(num, res = [], factor = 1) {
+    if (num) {
+      const val = (num % 10) * factor;
+      res.unshift(val);
+      return this.getNumberPlaceValue(Math.floor(num / 10), res, factor * 10);
+    }
+    return res;
+  }
+
   convertTens(digit) {
     const firstNumberOfDigit = digit.toString().split("")[0];
     const numberBase = Number(firstNumberOfDigit + "0");
