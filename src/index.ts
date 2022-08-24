7a94b3f36ebc6747f00c221329ea70d5b02f14c4
Symfony <omosekejiolawale@gmail.com>
2022-08-24 17:51:28 +0100

    Added type conversion and updated read.md

--- src/index.ts
+++ src/index.ts
@@ -52,14 +52,17 @@ export class Converter {
     return total;
   }
 
-  public convertDigits(digits: Array<number>): Array<number> {
-    let results: Array<number> = [];
+  public convertDigits(digits: Array<number>): Array<string> {
+    let results: Array<string> = [];
     if (digits && digits.length) {
       digits.forEach((digit) => {
         if (isNaN(digit)) {
           throw new Error(
             `All inputs must be a number :${digit} is not a NUMBER`
           );
+        }
+        if (digit >= 1000000) {
+          throw new Error("Can't convert numbers greater than 999,999");
         }
         results.push(this.convertDigit(digit));
       });
